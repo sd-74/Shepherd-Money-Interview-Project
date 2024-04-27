cafd0a6a3aabf121e75e5d41435cadcaef76b4d3
sd74 <sd74@illinois.edu>
2024-04-27 17:52:53 -0500

    Finished UserController

--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -3,10 +3,13 @@ package com.shepherdmoney.interviewproject.controller;
 import com.shepherdmoney.interviewproject.model.User;
 import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.CreateUserPayload;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 
 @RestController
@@ -19,9 +22,14 @@ public class UserController {
 
     @PutMapping("/user")
     public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
-        // TODO: Create an user entity with information given in the payload, store it in the database
-        //       and return the id of the user in 200 OK response
-        return null;
+        User user = new User();
+        user.setName(payload.getName());
+        user.setEmail(payload.getEmail());
+        userRepository.save(user);
+
+        // return the users id 
+        return ResponseEntity.ok(user.getId());
+
     }
 
     @DeleteMapping("/user")
@@ -29,6 +37,15 @@ public class UserController {
         // TODO: Return 200 OK if a user with the given ID exists, and the deletion is successful
         //       Return 400 Bad Request if a user with the ID does not exist
         //       The response body could be anything you consider appropriate
-        return null;
+        Optional<User> optionalUser = userRepository.findById(userId);
+
+        // return a 400 if a user w the id DNE
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.badRequest().body("user w id doesnt exist");
+        }
+        userRepository.deleteById(userId);
+
+        // return a ok response w the user id
+        return ResponseEntity.ok("adding user " + userId + " was succesful");
     }
 }
